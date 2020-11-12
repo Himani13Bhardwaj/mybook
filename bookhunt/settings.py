e3b46cbb6df8b89594cbbd11c0c14853238d7fe6
Avinash Kumar <avinash.kumar@algoscale.com>
2020-11-12 19:58:31 +0530

    image not show bug fix

--- bookhunt/settings.py
+++ bookhunt/settings.py
@@ -108,6 +108,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -163,5 +164,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+MEDIA_URL= "/media/"
--- bookhunt/urls.py
+++ bookhunt/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('api/userCollection/', include('usercollection.urls', 'usercollection_api')),
     path('api/userActivity/', include('useractivity.urls', 'useractivity_api')),
     path('api/userProfile/', include('userprofile.urls', 'userprofile_api')),
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
