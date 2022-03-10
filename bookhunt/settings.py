4675e7e5558f0097500e050d4e1da7dff09d156b
Himani13Bhardwaj <hrabhardwaj@gmail.com>
2022-03-10 18:46:25 +0530

    Update settings.py

--- bookhunt/settings.py
+++ bookhunt/settings.py
@@ -122,12 +122,12 @@ WSGI_APPLICATION = 'bookhunt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
 
 # DATABASES = {
 #     'default': {
